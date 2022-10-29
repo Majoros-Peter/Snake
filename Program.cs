3611e9b317365c1917a728efe93096feb2142760
Majoros-Peter <70958388+Majoros-Peter@users.noreply.github.com>
2022-10-29 19:16:56 +0200

    Update Program.cs
    Van hang  :D

--- Program.cs
+++ Program.cs
@@ -1,151 +1,159 @@
 using System;
-using System.Threading;
 using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
 
 class Snake
 {
-    public static Random rand = new Random();
-    public static string[] snake = { "12;6", "12;7", "12;8", "12;9", "12;10", "12;11" };
-    public static string point = "1;1";
-    public static bool grow = false;
-
-    public static void Main(string[] args)
+    public static char dir = 'u';
+    public static Random rnd = new Random();
+    public static byte width = 20, height = 11;
+    public static bool grow = false, game = true;
+    public static List<byte[]> snake = new List<byte[]>();
+    public static byte[] point = {Convert.ToByte(rnd.Next(1, width)), Convert.ToByte(rnd.Next(1, height))};
+    
+    static void Main(string[] args)
     {
-        string irany = "f";
-        byte width = Convert.ToByte(rand.Next(10, 31)), height = Convert.ToByte(rand.Next(10, 26));
-        point = spawnPoint(width, height);
+        Console.CursorVisible = false;
 
-        while (true)
+        for (byte i = 0; i < 3; i++)
         {
-            drawMap(width, height);         // Megrajzolja a kígyót.
-            irany = keyPressed(irany);      // Megvizsgálja, hogy melyik gomb van lenyomva.
-            snakeMove(irany);               // Mozog.
-            if (snake[0] == point)
+            byte[] l = {10, Convert.ToByte(i+5)};
+            snake.Add(l);
+        }
+        
+        Draw();
+
+        while(game)
+        {
+            if (Console.KeyAvailable) dir = KeyPressed();
+            Move();
+            if (snake[0][0] == point[0] && snake[0][1] == point[1])
             {
-                point = spawnPoint(width, height);
+                Console.Beep(2000, 200);
                 grow = true;
+                point = Spawnpoint();
             }
-            Thread.Sleep(100);              // 0.1 mp Késleltetés.
-            Console.Clear();                // Törli, hogy majd újra tudja rajzolni.
-            if (isSnakeAlive()) break;      // Megvizsgálja, hogy nem-e érintkezik a kígyónak valamelyik része.
+            Redraw();
+            Thread.Sleep(300);
         }
 
-        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Beep(3000, 200);
+        Console.Clear();
+        Console.SetCursorPosition(0, 0);
         Console.WriteLine("Vége");
     }
 
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    public static void drawMap(byte width, byte height)
+    static char KeyPressed()
     {
-        string coord;
-        for (byte y = 0; y < height; y++)
-            for (byte x = 0; x < width * 2; x++)
-            {
-                coord = Convert.ToString(x) + ";" + Convert.ToString(y);
-                Console.ForegroundColor = ConsoleColor.White;
-                if (y == 0 || y == height - 1) Console.Write("-");
-                else if (x == 0 || x == (width * 2) - 1) Console.Write("|");
-                else if (coord == point)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("*");
-                }
-                else if (coord == snake[0])
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("*");
-                }
-                else if (snake.Contains(coord))
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write("*");
-                }
-                else Console.Write(" ");
-                if (x == (width * 2) - 1) Console.WriteLine();
-            }
-    }
-
-
-    public static string keyPressed(string direction)
-    {
-        switch (Console.ReadKey().Key)
+        switch (Console.ReadKey(true).Key)
         {
             case ConsoleKey.UpArrow:
             case ConsoleKey.W:
-                return "f";
+                Console.Beep(500, 100);
+                return 'u';
             case ConsoleKey.DownArrow:
             case ConsoleKey.S:
-                return "l";
+                Console.Beep(500, 100);
+                return 'd';
             case ConsoleKey.LeftArrow:
             case ConsoleKey.A:
-                return "b";
+                Console.Beep(500, 100);
+                return 'l';
             case ConsoleKey.RightArrow:
             case ConsoleKey.D:
-                return "j";
+                Console.Beep(500, 100);
+                return 'r';
+            case ConsoleKey.Escape:
+                game = false;
+                return dir;
             default:
-                return direction;
+                return dir;
         }
-    }//todo needs fixing
+    }
 
-
-    public static void snakeMove(string direction)
+    static void Draw()
     {
-        string[] head = snake[0].Split(';');
-        byte szam;
-
-        switch (direction)
+        
+        for (byte i = 0; i < height; i++)
         {
-            case "f":
-                szam = Convert.ToByte(head[1]);
-                head[1] = Convert.ToString(szam - 1);
-                break;
-            case "l":
-                szam = Convert.ToByte(head[1]);
-                head[1] = Convert.ToString(szam + 1);
-                break;
-            case "j":
-                szam = Convert.ToByte(head[0]);
-                head[0] = Convert.ToString(szam + 1);
-                break;
-            case "b":
-                szam = Convert.ToByte(head[0]);
-                head[0] = Convert.ToString(szam - 1);
-                break;
+            Console.SetCursorPosition(0, i);
+            Console.Write('|');
         }
+        for (byte i = 0; i < height; i++)
+        {
+            Console.SetCursorPosition(width, i);
+            Console.Write('|');
+        }
+        for (byte i = 0; i < width + 1; i++)
+        {
+            Console.SetCursorPosition(i, 0);
+            Console.Write('-');
+        }
+        for (byte i = 0; i < width + 1; i++)
+        {
+            Console.SetCursorPosition(i, height);
+            Console.Write('-');
+        }
+        foreach(byte[] s in snake)
+        {
+            Console.SetCursorPosition(s[0], s[1]);
+            if (snake.IndexOf(s)==0) Console.ForegroundColor = ConsoleColor.Red;
+            else Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write('*');
+        }
+    }
+    
+    static void Move()
+    {
+        byte[] head = new byte[] {snake[0][0], snake[0][1]};
 
-        head[0] = head[0] + ";" + head[1];
-        head = head.Where(coord => coord != head[1]).ToArray();
+        if (dir == 'u') head[1]--;
+        else if (dir == 'd') head[1]++;
+        else if (dir == 'l') head[0]--;
+        else head[0]++;
+
+        snake.Insert(0, head);
 
         if (grow) grow = false;
-        else snake = (snake.Where(coord => coord != snake[snake.Length - 1]).ToArray());
-
-        snake = head.Concat(snake).ToArray();
+        else
+        {
+            Console.SetCursorPosition(snake[snake.Count-1][0], snake[snake.Count-1][1]);
+            Console.Write(" ");
+            snake.RemoveAt(snake.Count-1);
+        }
+        
+        if (IsSnakeAlive()) game = false;
     }
 
-
-    public static string spawnPoint(byte maxX, byte maxY)
+    static void Redraw()
     {
-        string x, y;
+        Console.SetCursorPosition(snake[0][0], snake[0][1]);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("*");
+        Console.SetCursorPosition(snake[1][0], snake[1][1]);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("*");
+        Console.SetCursorPosition(point[0], point[1]);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("*");
+    }
+
+    static byte[] Spawnpoint()
+    {
+        byte[] p = new byte[2];
         while (true)
         {
-            x = Convert.ToString(rand.Next(1, maxX * 2 - 2));       //A legkisebb érték 1, a legnagyobb maxX*2-3 lenne (keretek miatt)
-            y = Convert.ToString(rand.Next(1, maxY - 1));         //A legkisebb érték 1, a legnagyobb maxY-2 lenne (keretek miatt)
-            if (!snake.Contains(x + ";" + y)) break;
+            p = new byte[] {Convert.ToByte(rnd.Next(1,width)), Convert.ToByte(rnd.Next(1,height))};
+            if (!snake.Contains(p)) return p;
         }
-        return x + ";" + y;
     }
 
-
-    public static bool isSnakeAlive()
+    static bool IsSnakeAlive()
     {
-        foreach (string coord in snake)
-        {
-            if (snake.Count(s => s == coord) > 1)
-                return true;
-        }
+    for(int i = snake.Count-1; i > 0; i--)
+        if (snake[i][0] == snake[0][0] && snake[i][1] == snake[0][1] || (snake[0][0]==0 || snake[0][0] == width || snake[0][1] == 0 || snake[0][1] == height))
+            return true;
         return false;
     }
 }
